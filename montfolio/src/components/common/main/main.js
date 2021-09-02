5908b335c940e73cec4131685ca403ad589fe1db
monho <75512752+monho@users.noreply.github.com>
2021-09-02 16:30:04 +0900

    add : banner area
    add : banner area

--- montfolio/src/components/common/main/main.js
+++ montfolio/src/components/common/main/main.js
@@ -1,16 +1,28 @@
 import "./main.css";
+import React from 'react'
 
 const Main = () => {
+ 
+  
   return (
     <main id="contents" class="index-main">
       <section class="banner">
-        <div>
+        <div class="banner-area">
           <div class="text-area">
-            <span>"안녕하세요."</span>
+            <span class="title"><span class="word">항</span>상 흔적을 남기는</span>
+           
           </div>
+          <div class="effect-text">
+            <p> 개발자 문준호 입니다.</p><br></br>
+          </div>
+          <img class="logo" src="https://i.ibb.co/TPKwx9v/logo.png"></img>
+          
         </div>
       </section>
+      
     </main>
+
+    
   );
 };
 
