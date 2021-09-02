65facb243fcc0590ea4238284ffca1b640ced29e
문준호 <45911238+dcv215@users.noreply.github.com>
2021-09-02 10:01:32 +0900

    reset:project reinstall
    reset:project reinstall.

--- montfolio/src/App.js
+++ montfolio/src/App.js
@@ -1,17 +1,25 @@
-import React from "react";
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
-import * as P from "./pages/index";
-
-const App = () => {
+function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" component={P.mainPage} />
-        <Redirect path="*" to="/" />
-      </Switch>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
