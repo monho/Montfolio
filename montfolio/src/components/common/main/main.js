a8dbe95cb947790baff48e120aced45d13e7f597
문준호 <45911238+dcv215@users.noreply.github.com>
2021-09-03 16:08:43 +0900

    reset:main.js
    reset:main.js

--- montfolio/src/components/common/main/main.js
+++ montfolio/src/components/common/main/main.js
@@ -1,89 +1,65 @@
 import "./main.css";
 import React, { Component } from 'react';
-import TabContainer from './TabContainer/TabContainer';
-import Tab from './Tab/Tab';
-class Detail extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          index: 0,
-      }
-  }
 
-  updateIndex = index => {
-      this.setState({ index });
-  }
-  
-  render() {
-      const { index } = this.state;
-     
-      return (
-          <main id="contents" class="index-main">
-    <section class="banner">
-      <div class="banner-area">
-        <div class="text-area">
-          <span class="title"><span class="word">항</span>상 흔적을 남기는</span>
-         
-        </div>
-        <div class="effect-text">
-          <p> 개발자 문준호 입니다.</p><br></br>
-        </div>
-        <div class="effect-text2">
-        <span class="title"><span class="word">기</span>록하는 남자</span>
-        </div>
-        <img class="logo" src="https://i.ibb.co/TPKwx9v/logo.png"></img>
-        
-      </div>
-    </section>
-    <section class="my-skills" id="my-content">
-      <div>
-      <h2>My Skills</h2>
-      <p>제일 잘하는것들로 파트별로 나누어봤습니다.<br></br>저를 더 자세히 소개하기위해 준비한 프로젝트도 있습니다.</p>
-      <ul class="skill-img">
-        <li>
-          <div>
-            <img src="https://i.ibb.co/6Jwtyc3/frontend.png"></img>
-            <h4>Frontend</h4>
-            <p>
-              HTML5·CSS·JAVASCRIPT<br></br>React 개발
-            </p>
+const Main = () => { 
+  return (
+    <main id="contents" class="index-main">
+      <section class="banner">
+        <div class="banner-area">
+          <div class="text-area">
+            <span class="title"><span class="word">항</span>상 흔적을 남기는</span>
+           
           </div>
-        </li>
-        <li>
-          <div>
-            <img src="https://i.ibb.co/8c1FBBX/backend.png"></img>
-            <h4>Backend</h4>
-            <p>
-              Node.js·Express개발<br></br>Mysql DB 모델링
-            </p>
+          <div class="effect-text">
+            <p> 개발자 문준호 입니다.</p><br></br>
           </div>
+          <div class="effect-text2">
+          <span class="title"><span class="word">기</span>록하는 남자</span>
+          </div>
+          <img class="logo" src="https://i.ibb.co/TPKwx9v/logo.png"></img>
+          
+        </div>
+      </section>
+      <section class="my-skills" id="my-content">
+        <div>
+        <h2>My Skills</h2>
+        <p>제일 잘하는것들로 파트별로 나누어봤습니다.<br></br>저를 더 자세히 소개하기위해 준비한 프로젝트도 있습니다.</p>
+        <ul class="skill-img">
+          <li>
+            <div>
+              <img src="https://i.ibb.co/6Jwtyc3/frontend.png"></img>
+              <h4>Frontend</h4>
+              <p>
+                HTML5·CSS·JAVASCRIPT<br></br>React 개발
+              </p>
+            </div>
           </li>
-      </ul>
-      </div>
+          <li>
+            <div>
+              <img src="https://i.ibb.co/8c1FBBX/backend.png"></img>
+              <h4>Backend</h4>
+              <p>
+                Node.js·Express개발<br></br>Mysql DB 모델링
+              </p>
+            </div>
+            </li>
+        </ul>
+        </div>
+        
+      </section>
+      <section class="my-project" id="my-content">
+        <div>
+        <h2>My Project</h2>
       
-    </section>
-    <section class="my-project" id="my-content">
-      <div>
-      <h2>My Project</h2>
-      <TabContainer index={index} updateIndex={this.updateIndex}>
-        <Tab title="개인 프로젝트" className="listBox">
-          <Explicate />
-        </Tab>
-        <Tab title="팀 프로젝트" className="listBox">
-          <ReviewCmt />
-        </Tab>
-       </TabContainer> 
-    
-      </div>
-    </section>
+        </div>
+      </section>
+
+      
+    </main>
 
     
-  </main>
-      );
-  }
-};
-
-export default Detail;
+  );
+}
   
   
  
