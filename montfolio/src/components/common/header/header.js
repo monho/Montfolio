ba95132fd568f777a3a80151373f75fe0888654b
문준호 <45911238+dcv215@users.noreply.github.com>
2021-09-08 12:10:19 +0900

    .
    .

--- montfolio/src/components/common/header/header.js
+++ montfolio/src/components/common/header/header.js
@@ -1,28 +1,53 @@
 import "./header.css";
 import React from 'react'
-import { useMediaQuery } from 'react-responsive'
+import styled from 'styled-components'
+import MONTFLOIO from './header-img/logo.png'
 const Header = () => {
     return (
-      <header>
-            <div>
-              <h1>
-                <a href="/">
-                {/* 로고 */}
-                  <img src="img/header-img/logo.png"></img> 
-                </a>
-              </h1>
-              <nav class="lnb menu">
-	              <ul>
-                  <li><a href="/about" class="">내소개</a></li>
-                  <li><a href="/project" class="">프로젝트</a></li>
-                  <li><a href="https://github.com/monho" target="_blank">GITHUB</a></li>
-	            </ul>
-                
-	            </nav>
-            </div>
-      </header>
+      <Common>
+        <LogoImg src={MONTFLOIO}/>
+
+        <ContlorBox >
+          <Contlor href='/connect'>내 소개</Contlor>
+          <Contlor href='/login'>프로젝트</Contlor>
+          <Contlor href='/signup'>GITHUB</Contlor>
+      </ContlorBox>
+      </Common>
     );
   };
 
+  const Common = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin: 20px 210px;
+    @media screen and (max-width: 500px) {
+      flex-direction: column;
+  }
+    `
+
+
+
+    const LogoImg = styled.img`
+`
+
+
+const ContlorBox = styled.div`
+    display: flex;
+    margin-right:60%;
+    align-items:center;
+    line-height: 10%;
+    @media screen and (max-width: 500px) {
+        
+      flex-direction: column;
+      align-items:flex-end;
+      display: none;
+  }
+`
+
+const Contlor = styled.a`
+    margin: 10px;
+    text-decoration: none;
+    color: black;
+    `
   export default Header;
   
